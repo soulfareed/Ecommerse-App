53a2396d54285636f59c12caf23e95c1d2c08f17
fareed <fareedzaman501@gmail.com>
2022-09-17 09:03:11 +0530

    new codes

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="hero">
+      <div className="card text-bg-dark border-0">
+        <img
+          src="/assets/bg.jpg"
+          className="card-img"
+          alt="Background"
+          height={600}
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              NEW SEASON ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">Check Out All The Trends</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
