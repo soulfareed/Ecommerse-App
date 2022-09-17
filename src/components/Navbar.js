e3012168192dcd0e37526c6436f60c36f7d2ae15
fareed <fareedzaman501@gmail.com>
2022-09-17 09:03:11 +0530

    new codes

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light bg-white py-3 shadow-sm">
+        <div className="container-fluid">
+          <a className="navbar-brand fw-bold fs-4" href="#">
+            LA collection
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Products
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contacts
+                </a>
+              </li>
+            </ul>
+            <div className="buttons">
+              <button className="btn">
+                <a href="#" className="btn btn-outline-dark">
+                  <i className="fa fa-sign-in me-1"></i> Login
+                </a>
+                <a href="#" className="btn btn-outline-dark ms-2">
+                  <i className="fa fa-user-plus me-1"></i> Register
+                </a>
+                <a href="#" className="btn btn-outline-dark ms-2">
+                  <i className="fa fa-shopping-cart me-1"></i> Cart(0)
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
